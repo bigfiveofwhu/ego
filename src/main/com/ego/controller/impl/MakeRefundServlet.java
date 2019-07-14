7ccbce1f8e493ebbf4ae399a406ce22a592ba4fa
seizerccc <907839891@qq.com>
2019-07-14 22:00:34 +0800

    历史,积分,申诉

---
+++ src/main/com/ego/controller/impl/MakeRefundServlet.java
@@ -0,0 +1,13 @@
+package com.ego.controller.impl;
+
+public class MakeRefundServlet extends CommentControllerSupport 
+{
+
+	@Override
+	public String execute() throws Exception 
+	{
+		
+		return "WEB-INF/views/makeRefund";
+	}
+
+}
