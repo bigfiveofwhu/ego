419e241722d4ef15657f1d5c200f13b01accffdb
Obsessionhg <2357612757@qq.com>
2019-07-20 10:47:02 +0800

    更新github

--- src/main/com/ego/controller/impl/CreateOrderServlet.java
+++ src/main/com/ego/controller/impl/CreateOrderServlet.java
@@ -1,10 +1,11 @@
 package com.ego.controller.impl;
 
-public class CreateOrderServlet extends OrderControllerSupport {
+public class CreateOrderServlet extends OrderControllerSupport
+{
 
 	@Override
-	public String execute() throws Exception {
-		// TODO Auto-generated method stub
+	public String execute() throws Exception
+	{
 		return "WEB-INF/views/createOrder";
 	}
 
